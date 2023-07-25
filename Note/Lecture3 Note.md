8088d624caeda317f1131f58642482d3f51344c7
Amy <dingxiaoyu168@outlook.com>
2023-07-26 00:35:16 +0800

    test markdown note

---
+++ Note/Lecture3 Note.md
@@ -0,0 +1,4 @@
+并发 VS 并行
+
+线程 VS 进程
+![Alt text](image-3.png)
--- example1-guessnum.go
+++ example1-guessnum.go
@@ -18,17 +18,19 @@ func main(){
 
 	fmt.Println("Please input your guess")
 	reader := bufio.NewReader(os.Stdin)
-	input,err :=reader.ReadString('\n')
-	if err!=nil{
-		fmt.Println("An err ocurred while reading input. Please try again",err)
-		return
-	}
-	input = strings.TrimSuffix(input,"\n")
+	for{
+		input,err :=reader.ReadString('\n')
+		if err!=nil{
+			fmt.Println("An err ocurred while reading input. Please try again",err)
+			return
+		}
+		input = strings.TrimSuffix(input,"\n")
 
-	guess,err:=strconv.Atoi(input)
-	if err!=nil{
-		fmt.Println("Invalid input. Please enter an integer value")
-		return
+		guess,err:=strconv.Atoi(input)
+		if err!=nil{
+			fmt.Println("Invalid input. Please enter an integer value")
+			return
+		}
+		fmt.Println("Your guess is",guess)
 	}
-	fmt.Println("Your guess is",guess)
 }
